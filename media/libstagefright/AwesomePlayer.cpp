0c50b787f4ef1cd5aca65006e6dd1411b7e19957
Suresh Choudhary <sureshc@nvidia.com>
2014-05-02 19:04:27 +0530

    AwesomePlayer: Handle unsupported tracks
    Change-Id: I0d359e704200d86be237211dc816464f7186080b

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -381,7 +381,9 @@ status_t AwesomePlayer::setDataSource_l(const sp<MediaExtractor> &extractor) {
     mExtractor = extractor;
     for (size_t i = 0; i < extractor->countTracks(); ++i) {
         sp<MetaData> meta = extractor->getTrackMetaData(i);
-
+        if (meta == NULL) {
+            continue;
+        }
         int32_t bitrate;
         if (!meta->findInt32(kKeyBitRate, &bitrate)) {
             const char *mime;
@@ -411,7 +413,9 @@ status_t AwesomePlayer::setDataSource_l(const sp<MediaExtractor> &extractor) {
     bool haveVideo = false;
     for (size_t i = 0; i < extractor->countTracks(); ++i) {
         sp<MetaData> meta = extractor->getTrackMetaData(i);
-
+        if (meta == NULL) {
+            continue;
+        }
         const char *_mime;
         CHECK(meta->findCString(kKeyMIMEType, &_mime));
 
