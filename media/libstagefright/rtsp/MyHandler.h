351b4c2e454469a57a150feb08c3fb4518f7afa4
Oscar Rydhé <oscar.rydhe@sonymobile.com>
2015-01-30 14:49:39 +0100

    Abort RTSP setup if abort has been called
    If the RTSP session is aborted between the sending and
    receiving of the setup call the track would use uninitialized
    sockets, leading to a crash. This fix will check if the track
    is valid before proceeding with the setup.
    
    Change-Id: Idab76c69728f767e775ef9bc5450f23c042551d3

--- media/libstagefright/rtsp/MyHandler.h
+++ media/libstagefright/rtsp/MyHandler.h
@@ -645,7 +645,12 @@ struct MyHandler : public AHandler {
                 TrackInfo *track = NULL;
                 size_t trackIndex;
                 if (msg->findSize("track-index", &trackIndex)) {
-                    track = &mTracks.editItemAt(trackIndex);
+                    if (mTracks.size() > 0) {
+                        track = &mTracks.editItemAt(trackIndex);
+                    } else {
+                        ALOGW("Track not found. Abort SETUP(%d)", index);
+                        return;
+                    }
                 }
 
                 int32_t result;
