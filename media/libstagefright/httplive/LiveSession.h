b0e3dfa6ff67833e27bbb5c063df61cde5a002aa
Oscar Rydhé <oscar.rydhe@sonymobile.com>
2015-02-25 16:45:04 +0100

    Implement ReconfigurationState in LiveSession
    Reconfiguration can be triggered by select track, seek and
    bandwidth changes. Currently it'ss only a boolean variable used
    to handle these reconfigurations but that is not always enough.
    
    This commit introduces a state variable with the values:
            -NONE
            -RECONFIGURATION_PENDING
            -RECONFIGURATION_IN_PROGRESS
            -DISCONNECT
    
    It will now be much easier to allow or reject the reconfiguration
    depending on which state previous rencofiguration is in.
    
    Change-Id: I52f31fde1cf3f09f3cb4152c45e4e86e1bb3f7c4

--- media/libstagefright/httplive/LiveSession.h
+++ media/libstagefright/httplive/LiveSession.h
@@ -101,6 +101,7 @@ private:
         kWhatConnect                    = 'conn',
         kWhatDisconnect                 = 'disc',
         kWhatSeek                       = 'seek',
+        kWhatSelectTrack                = 'strk',
         kWhatFetcherNotify              = 'notf',
         kWhatChangeConfiguration        = 'chC0',
         kWhatChangeConfiguration2       = 'chC2',
@@ -195,7 +196,13 @@ private:
     int64_t mLastDequeuedTimeUs;
     int64_t mRealTimeBaseUs;
 
-    bool mReconfigurationInProgress;
+    enum ReconfigurationState {
+        NONE,
+        RECONFIGURATION_PENDING,
+        RECONFIGURATION_IN_PROGRESS,
+        DISCONNECTING
+    };
+    ReconfigurationState mReconfigurationState;
     bool mSwitchInProgress;
     uint32_t mDisconnectReplyID;
     uint32_t mSeekReplyID;
@@ -267,6 +274,8 @@ private:
     bool canSwitchUp();
     bool isReconfiguring();
     bool bandwidthChanged();
+
+    void onSelectTrack(const sp<AMessage> &msg);
 
     DISALLOW_EVIL_CONSTRUCTORS(LiveSession);
 };
